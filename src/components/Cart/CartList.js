4843bf6d5fbde658ecebe07d010fb0a34b0849d8
maheswari308 <maheswari@cookiestechnologies.com>
2023-10-27 23:11:42 +0530

    minor issue fiexed

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CartItem from './CartItem';
 
 export default function CartList({value}) {
-    const { cart} =value;
+    const { cart} = value;
    
     return (
      <div className="container-fluid">
